13e3734d614e66310ee2d1969406e50de5ee8743
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2024-07-17 22:09:49 +0200

    make everything private that is not needed

--- src/testor/core.clj
+++ src/testor/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str]
             [clojure.pprint :as pp]))
 
-(defn gen-test 
+(defn- gen-test 
   ([form] (gen-test form (eval form)))
   ([form expected]
   `(~'deftest ~(symbol (str (name (first form)) "-test-" (quot (System/currentTimeMillis) 1000) "-" (name (gensym ""))))
@@ -17,20 +17,20 @@
        "_test"
        ".clj"))
 
-(defn get-test-file []
+(defn- get-test-file []
   (clojure.java.io/file "test/" (ns-file-name (ns-name *ns*))))
 
-(defn add-test! [macroexpanded-test]
+(defn- add-test! [macroexpanded-test]
   (let [file (get-test-file)]
     (when (.exists file)
       (spit file (str \newline (with-out-str (pp/pprint macroexpanded-test))) :append true))))
 
-(defn gen-test-ns []
+(defn- gen-test-ns []
   `(~'ns ~(symbol (str (ns-name *ns*) "-test"))
      (:require [~'clojure.test :refer :all]
                [~(ns-name *ns*) :refer :all])))
 
-(defn add-test! [macroexpanded-test]
+(defn- add-test! [macroexpanded-test]
   (let [file (get-test-file)]
     (when-not (.exists file)
       (.mkdirs (.getParentFile file))
